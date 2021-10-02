fad2cf274b37e2ed74347f7abcf57ce19b46babc
edisson <osorioedisson08@gmail.com>
2021-10-01 22:12:00 -0500

    [FIX]5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - print numbers
+*
+* Return: 0
+*/
+int main(void)
+{
+int numero = 9;
+int aux = 0;
+while (aux <= numero)
+{
+printf("%d", aux);
+aux++;
+}
+printf("\n");
+return (0);
+}
