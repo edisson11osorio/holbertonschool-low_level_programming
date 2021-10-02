f163e9be23ed90525f9e10659974f6a147b97ed6
edisson <osorioedisson08@gmail.com>
2021-10-01 22:05:11 -0500

    [ADD_puts]2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i;
+i = 'a';
+while (i <= 'z')
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
