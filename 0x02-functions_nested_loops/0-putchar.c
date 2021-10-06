a8d8b19be9d61ae5776c467d624c2f7868d86bdf
edisson <osorioedisson08@gmail.com>
2021-10-05 20:02:59 -0500

    [FIX :C]0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
 #include "main.h"
-
 int main(void)
 {
-    /**
- *main - print_holberton
- *Return: 0
+/**
+*main - print_holberton
+*Return: 0
 */
-	_putchar(95);
-	_putchar(112);
-	_putchar(117);
-	_putchar(116);
-	_putchar(99);
-	_putchar(104);
-	_putchar(97);
-	_putchar(114);
-    _putchar('\n');
-    return (0);
+_putchar(95);
+_putchar(112);
+_putchar(117);
+_putchar(116);
+_putchar(99);
+_putchar(104);
+_putchar(97);
+_putchar(114);
+_putchar('\n');
+return (0);
 }
