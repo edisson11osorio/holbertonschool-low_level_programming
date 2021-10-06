cef945232d2da4cfa4ec334660202ed2717be11e
edisson <osorioedisson08@gmail.com>
2021-10-05 23:30:48 -0500

    [FIX]9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- *  times_table - print table 9
- */
+ * times_table - print table 9
+*/
 void times_table(void)
 {
 int a, b, z;
 for (a = 0; a <= 9; a++)
 {
 for (b = 0; b <= 9; b++)
+{
 z = a * b;
 if (b != 0)
 {
@@ -20,12 +21,14 @@ _putchar((z / 10) + '0');
 _putchar((z % 10) + '0');
 }
 else if
-((z < 10) && (z != 0))
+((z < 10) && (b != 0))
 {
 _putchar(' ');
 _putchar((z % 10) + '0');
 }
 else
 _putchar((z % 10) + '0');
+}
+_putchar('\n');
 }
 }
