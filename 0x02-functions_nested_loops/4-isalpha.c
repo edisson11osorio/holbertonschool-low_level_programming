c28c9244e77f6e07639735ea29ee5718567f949c
edisson <osorioedisson08@gmail.com>
2021-10-05 21:13:50 -0500

    [ADD]4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * _isalpha - letter (M-m)
+ * @c: the charter checked
+ * Return: 1 is letter, 0 otherwise
+ */ 
 int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
