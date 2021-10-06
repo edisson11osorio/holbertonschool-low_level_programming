96cce1d00660919194a042b3419fb49e41a70b85
edisson <osorioedisson08@gmail.com>
2021-10-05 20:16:47 -0500

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - function_alphabet
+ * new line
+ */
+void print_alphabet(void)
+{
+int letter = 97;
+while (letter <= 122)
+{
+_putchar(letter);
+letter++;    
+}
+_putchar('\n');    
+}
