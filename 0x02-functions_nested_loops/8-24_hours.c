58d197cf31aca9b9b3e472a1b1005ab3323bc0c0
edisson <osorioedisson08@gmail.com>
2021-10-05 22:49:40 -0500

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - houes and min 
+ * 23:59
+ */
+
+void jack_bauer(void)
+{
+int hour;
+int min;
+for (hour = 0; hour <= 23; hour++)
+{
+for (min = 0; min <= 59; min++)    
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
+_putchar('\n');
+}
+}
+}
