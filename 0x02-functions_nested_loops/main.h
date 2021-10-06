7a586a1496a265c9701bd95bfdf331400fdfab4f
edisson <osorioedisson08@gmail.com>
2021-10-05 19:52:27 -0500

    files

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+/*
+* File: main.h
+* Desc: Header file containing declarations for all functions
+* used in the 0x02-functions_nested_loops directory.
+*/
+int _putchar(char c);
+void print_alphabet(void);
