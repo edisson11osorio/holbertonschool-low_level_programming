4e7e50e8b13e0c0fe63aa1588d5079cf5ddd286a
edisson <osorioedisson08@gmail.com>
2021-10-05 23:16:26 -0500

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *  times_table - print table 9
+ */
+void times_table(void)
+{
+int a, b, z;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+z = a * b;
+if (b != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+if (z >= 10)
+{
+_putchar((z / 10) + '0');
+_putchar((z % 10) + '0');
+}
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,3 +12,4 @@ int print_sign(int n);
 int _abs(int num);
 int print_last_digit(int n);
 void jack_bauer(void);
+void times_table(void);
