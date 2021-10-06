e3d60261c7e9459532124a4b599d35dd59f45c26
edisson <osorioedisson08@gmail.com>
2021-10-05 23:35:51 -0500

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - add a+b
+ * @num1: numero a
+ * @num2: numero b
+ * Return: a +b
+ */
+int add(int num1, int num2)
+{
+int suma;
+suma = num1 + num2;
+return (suma);
+}
