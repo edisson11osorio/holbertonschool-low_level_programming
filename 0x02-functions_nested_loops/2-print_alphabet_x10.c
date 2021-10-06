575adc406373322e75999e81e874ea3b00df50f5
edisson <osorioedisson08@gmail.com>
2021-10-05 20:45:19 -0500

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 10 times
+ * print 10 times
+*/
+void print_alphabet_x10(void)
+{
+int num = 0;
+char caracter;
+while (num++ <=9)
+{
+for (caracter = 'a'; caracter = 'z'; caracter++)
+_putchar(caracter);
+_putchar('\n');    
+}    
+}
