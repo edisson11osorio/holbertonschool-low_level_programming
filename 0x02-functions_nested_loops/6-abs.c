0aef01b6faf50fda0c2a15835fa99ce618edb3ba
edisson <osorioedisson08@gmail.com>
2021-10-05 22:13:07 -0500

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_abs - print absolute number
+ * @num: number to be checked
+ * Return: absolute value
+ */
+int _abs(int num)
+{
+if (num >= 0)
+return (num);
+else
+return (-num);
+}
