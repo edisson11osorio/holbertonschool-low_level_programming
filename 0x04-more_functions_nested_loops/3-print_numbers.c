ffbcae0158c43cea591ea0b1877a2953db50c2f8
edisson <osorioedisson08@gmail.com>
2021-10-08 13:07:38 -0500

    betty

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,10 +7,10 @@
 
 void print_numbers(void)
 {
-    int n;
-    for (n = 0; n < 10; n++)
-    {
-        _putchar(n);
-        _putchar('\n');
-    }
+int n;
+for (n = 0; n < 10; n++)
+{
+_putchar(n);
+_putchar('\n');
+}
 }
