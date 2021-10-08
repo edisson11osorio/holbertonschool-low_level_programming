3021a5d6f55abecccd8c655104dc3891bf6fa5db
edisson <osorioedisson08@gmail.com>
2021-10-08 13:08:58 -0500

    [FIX_CODE]

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_numbers - print the numbers from 0 to 9
- * new line
+ *print_numbers - prints the numbers
+ *
+ *Description: Prints the numbers from 0 through 9
+ *Return: void
  */
-
 void print_numbers(void)
 {
 int n;
