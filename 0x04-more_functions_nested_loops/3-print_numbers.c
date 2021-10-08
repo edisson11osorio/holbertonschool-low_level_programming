318c0e734b4436c65fdc37b064bbfae77dac209e
edisson <osorioedisson08@gmail.com>
2021-10-08 13:03:20 -0500

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * 
+ * 
+ */
+
+void print_numbers(void)
+{
+    int n;
+    for (n = 0; n < 10; n++)
+    {
+        _putchar(n);
+        _putchar('\n');
+    }
+}
