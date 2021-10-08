a5917b77d7b11853be065ef1fa94f3dbe77c4a27
edisson <osorioedisson08@gmail.com>
2021-10-08 12:13:45 -0500

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * 
+ * 
+*/ 
+
+int _isdigit(int c)
+{
+    for ( c = 0; c < 10 ; c++)
+    {
+        return (1);
+    else
+        return (0);
+    }
+    
+}
+
+
