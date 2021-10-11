0bb70eab1e2c7d3a5d999f66e35b0690d917d7f0
edisson <osorioedisson08@gmail.com>
2021-10-11 15:42:13 -0500

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swap values
+ * @a : pointer value
+ * @b : pointer value
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+    int x;
+
+    x = *a;
+    *a = *b;
+    *b = x;
+}
