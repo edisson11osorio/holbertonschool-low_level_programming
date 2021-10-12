f68fa8fdea75575ee36a9360243fb8b13c025cb1
edisson <osorioedisson08@gmail.com>
2021-10-11 23:30:18 -0500

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse strings
+ * @s : string variable
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int a = 0, b = 0;
+	char c, d;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	a--;
+	for (b = 0; b < a; b++)
+	{
+		c = s[b];
+		d = s[a];
+		s[b] = d;
+		s[a] = c;
+		a--;
+	}
+}
+
