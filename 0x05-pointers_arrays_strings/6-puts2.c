eb54e8509cd83516e2d74032e45163e037bf0d47
edisson <osorioedisson08@gmail.com>
2021-10-11 23:30:36 -0500

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - skipping digits in array
+ * @str : array defined
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0; *(str + a) != '\0'; a++)
+	{
+		if (a % 2 == 0)
+			_putchar(*(str + a));
+	}
+	_putchar('\n');
+}
