0e2463e3eb528036be2406808866c072af3a066b
edisson <osorioedisson08@gmail.com>
2021-10-14 13:33:14 -0500

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: string 1
+ * @src: string 2
+ *
+ * Return: char
+ */
+char *_strcat(char *dest, char *src)
+{
+	int k;
+	int i;
+
+	for (k = 0; dest[k] != '\0'; k++)
+		;
+	for (i = 0; src[i] != '\0'; i++)
+		dest[k++] = src[i];
+	dest[k] = '\0';
+	return (dest);
+}
