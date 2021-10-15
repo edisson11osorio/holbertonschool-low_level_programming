0d2b8fddfda81af401bd66ce61c2d10d9a68cc50
edisson <osorioedisson08@gmail.com>
2021-10-14 23:19:53 -0500

    [ADD]changes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to be capitalized
+ *
+ * Return: char
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] = s[0] - 32;
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == '\t' || s[i] == '\n' || s[i] == ' ' || s[i] == '!'
+		    || s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i]
+		    == ',' || s[i] == '.' || s[i] == ';' || s[i] == '?'
+		    || s[i] == '{' || s[i] == '}')
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				s[i + 1] = s[i + 1] - 32;
+	return (s);
+}
