713bf000d780ade2ec54f8def9bed037cdf62ef5
edisson <osorioedisson08@gmail.com>
2021-10-21 23:10:33 -0500

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_puts_recursion - pirnts a string recursively
+ *@s: string to print
+ *Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
