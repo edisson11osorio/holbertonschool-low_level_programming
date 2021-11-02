96be4d7488029377b46523baf418c6d1daeba04f
edisson <osorioedisson08@gmail.com>
2021-11-01 23:49:58 -0500

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main- Program printsyhe name of the file
+*    using common predefined macros
+* Return: 0
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
