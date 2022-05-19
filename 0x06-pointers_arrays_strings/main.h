d53ae97dce896475847f4f39d8f7cca55c2d83db
edisson11osorio <osorioedisson08@gmail.com>
2022-05-19 17:26:51 -0500

    fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,14 +1,18 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *n);
-char *cap_string(char *n);
-char *leet(char *n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *, char *, char *, int size_r);
 
-#endif /* HOLBERTON_H */
+#endif
