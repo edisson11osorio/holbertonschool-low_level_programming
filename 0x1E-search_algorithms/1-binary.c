6aee877737292de30637f1dc05455ba829479f95
edisson11osorio <osorioedisson08@gmail.com>
2022-06-12 22:45:41 -0500

    [✅] check and pass code

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,4 @@
 #include "search_algos.h"
-
 /**
   * binary_search_recursive - Searches recursively for a value in a sorted
   *                           array of integers using binary search.
@@ -11,8 +10,8 @@
   * Return: If the value is not present, -1.
   *         Otherwise, the index where the value is located.
   */
-  int binary_search(int *array, size_t size, int value);
-  {
+int binary_search_recursive(int *array, size_t left, size_t right, int value)
+{
 	size_t i;
 
 	if (right < left)
