c086b9495b815054c2cb62a9f60b1b6713ee1408
edisson11osorio <osorioedisson08@gmail.com>
2022-06-12 22:39:18 -0500

    [✅] fix code

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,20 +10,19 @@
   * Return: If the value is not present or the array is NULL, -1.
   *         Otherwise, the first index where the value is located.
   */
-
-int linear_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
+	size_t i;
 
-    if(array == NULL)
-         return (-1);
+	if (array == NULL)
+		return (-1);
 
-    for(i = 0; i < size; i++)
-    {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-              return (i);
-    }
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
 
-    return (-1);
+	return (-1);
 }
